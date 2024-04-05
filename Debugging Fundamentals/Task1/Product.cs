67f7f9400fe844dde57e5298fecbb33b66ecba11
Aliaksei Dudko <aliaksei_dudko1@epam.com>
2024-04-05 14:59:00 +0300

    add HW Module4

---
+++ Debugging Fundamentals/Task1/Product.cs
@@ -0,0 +1,32 @@
+﻿namespace Task1
+{
+    public class Product
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var product = obj as Product;
+            if(product is null)
+            {
+                return false;
+            }
+
+            return this.Name == product.Name &&
+                    this.Price == product.Price;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
